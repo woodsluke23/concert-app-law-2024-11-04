e961009b06f7be52f54ec106dfaade5a84d065f3
Luke Woods <woodsluke23@gmail.com>
2024-11-04 20:41:40 +0000

    Updated HTML

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 let express = require("express");
-let app = new express();                                                                     
-
+let app = new express();
 // set up database connection
 const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
-  port: 3306,
- },
+client: "mysql",
+connection: {
+host:"concert-db.cvyykm4m8pnf.us-east-2.rds.amazonaws.com",
+user: "admin",
+password: "password1",
+database:"paradise-concerts",
+port: 3306,
+},
 });
-
 app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
- .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+knex
+.select()
+.from("venues")
+.then((result) => {
+let html = "<body><ul>";
+for (let i=0;i<result.length;i++) {
+html += "<li>" + result[i].location + "</li>";
+}
+html += "</body>"
+res.send(html);
+});
 });
 app.listen(3000);
